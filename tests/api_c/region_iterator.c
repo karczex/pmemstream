d44b3dae24e576311fd969fcbebcd6ee12a8cc32
Pawel Karczewski <pawel.karczewski@intel.com>
2022-04-11 19:30:41 +0200

    Let's iterate over freelist of regions

--- tests/api_c/region_iterator.c
+++ tests/api_c/region_iterator.c
@@ -38,6 +38,62 @@ void valid_input_test(char *path)
 	pmemstream_test_teardown(env);
 }
 
+void remove_region_while_holding_iterator_test(char *path)
+{
+	struct pmemstream_test_env env;
+	env.map = map_open(path, TEST_DEFAULT_STREAM_SIZE * 10, true);
+	int ret = pmemstream_from_map(&env.stream, TEST_DEFAULT_BLOCK_SIZE, env.map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region_iterator *riter;
+
+	struct pmemstream_region regions[3];
+	for (size_t i = 0; i < 3; i++) {
+		int ret = pmemstream_region_allocate(env.stream, TEST_DEFAULT_REGION_SIZE, &regions[i]);
+		UT_ASSERTeq(ret, 0);
+	}
+	ret = pmemstream_region_iterator_new(&riter, env.stream);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(riter, NULL);
+
+	/* Go to 2nd region */
+	struct pmemstream_region region;
+	ret = pmemstream_region_iterator_next(riter, &region);
+	UT_ASSERTeq(ret, 0);
+
+	/* Free all regions */
+	for (size_t i = 0; i < 3; i++) {
+		int ret = pmemstream_region_free(env.stream, regions[i]);
+		UT_ASSERTeq(ret, 0);
+	}
+
+	struct pmemstream_region region_to_preserve;
+	struct pmemstream_region region_to_remove;
+
+	ret = pmemstream_region_allocate(env.stream, TEST_DEFAULT_REGION_SIZE, &region_to_preserve);
+	UT_ASSERTeq(ret, 0);
+
+	ret = pmemstream_region_allocate(env.stream, TEST_DEFAULT_REGION_SIZE, &region_to_remove);
+	UT_ASSERTeq(ret, 0);
+
+	ret = pmemstream_region_free(env.stream, region_to_remove);
+	UT_ASSERTeq(ret, 0);
+
+	/* let's iterate over free list */
+	struct pmemstream_region region_from_free_list;
+	ret = pmemstream_region_iterator_next(riter, &region_from_free_list);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTeq(region_from_free_list.offset, regions[1].offset);
+
+	ret = pmemstream_region_iterator_next(riter, &region_from_free_list);
+	UT_ASSERTeq(region_from_free_list.offset, region_to_remove.offset);
+
+	pmemstream_region_iterator_delete(&riter);
+	UT_ASSERTeq(riter, NULL);
+
+	pmemstream_test_teardown(env);
+}
+
 void null_iterator_test(char *path)
 {
 	pmemstream_test_env env = pmemstream_test_make_default(path);
@@ -98,7 +154,7 @@ int main(int argc, char *argv[])
 	START();
 
 	char *path = argv[1];
-
+	remove_region_while_holding_iterator_test(path);
 	valid_input_test(path);
 	null_iterator_test(path);
 	invalid_region_test(path);
